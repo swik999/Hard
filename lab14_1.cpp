d1a0daad1cd2e18f36cf6aee1cd9fd33ca2ef2dd
Suwat Inkaew <swik999@gmail.com>
2021-02-02 18:47:10 +0700

    n

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,7 +1,30 @@
-include <iostream>
+#include<iostream>
 using namespace std;
 
 int main(){
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	char *y = &b;
+	int **z = &x;
+	
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
+	
+	cout << &a << " " << &b << " " << &c << " " << &x << " " << &y << " " << &z << "\n";
+	
+	c = 'F';
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
+	
+	*y = 'W';
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
+	
+	*x = 6;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
+	
+	**z = 7;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
+	
 
 	
 	
